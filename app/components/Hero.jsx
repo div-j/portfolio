bfcfb36f64339f7eb665ef667908bdc60cc406cf
div-j <divinejohn65@gmail.com>
2025-03-03 17:45:19 +0100

    v1

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
             <Image
               src="/images/about-image.png"
               alt="hero image"
-              className="absolute  transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
             />
